6bd5508e75f44112c43700e395496792c746f266
yuan <zu00423828@gmail.com>
2021-09-07 12:23:13 +0800

    modify read image dir process

--- dataflow.py
+++ dataflow.py
@@ -1,16 +1,15 @@
+from sys import path
 import cv2
 import  numpy as np
 import face_alignment
 import math
 import os
 import subprocess
-
-
+import shutil
+DEVNULL = open(os.devnull, 'wb')
 #parpamerters:dict
-
-
 fa = face_alignment.FaceAlignment(
-    face_alignment.LandmarksType._2D, flip_input=False, face_detector="blazeface", device="cpu")
+    face_alignment.LandmarksType._2D, flip_input=False, face_detector="blazeface", device="cuda")
 
 class DownloadException(Exception):
     def __init__(self):
@@ -18,23 +17,30 @@ class DownloadException(Exception):
 class ValidException(Exception):
     def __init__(self) -> None:
         super().__init__("is not valid")
-def download(uri):
+def download(uri,parameters):
+    print(uri)
     id=uri.rsplit("https://youtube.com/watch?v=")[-1]
     try:
+        save_path=f"tmp/video/{id}.mp4"
         subprocess.run(f"youtube-dl '{uri}'  -f 136+140 -o 'tmp/video/{id}.mp4'",shell=False,check=True)
     except Exception :
-        print("發生錯誤")
+        print("沒有1080p，或無法下載")
+        raise DownloadException()
+    return save_path,parameters
 def avspeech_preprocess(data:str,args:dict):
-    id,start,end=[]
+    id,start,end=[0,3,10]
     path=os.path.join(f"tmp/video",data)
     os.makedirs(data,exist_ok=True)
-    subprocess.run(f"ffmpeg -i '{data} --ss {start} -to {end} {path}/%5d.png'")
-    subprocess.run(f"ffmpeg -i '{data} --ss {start} -to {end} {path}/audio.wav'")
+    subprocess.run(f"ffmpeg -i '{data}' -ss {start} -to {end} '{path}/%5d.png'")
+    subprocess.run(f"ffmpeg -i '{data}' -ss {start} -to {end} '{path}/audio.wav'")
 def voxceleb2_preproces(data: str,args:dict):
-    id,start,end,x,y=[]
-    path=os.path.join(f"tmp/video",data)
-    subprocess.run(f"ffmpeg -i '{data} --ss {start} -to {end} {path}/%5d.png'")
-    subprocess.run(f"ffmpeg -i '{data} --ss {start} -to {end} {path}/audio.wav'")
+    id,start,end,x,y=[0,3,10,256,256]
+    filename=data.rsplit(".mp4")[0]
+    os.makedirs(filename,exist_ok=True)
+    path=os.path.join(f"tmp/video",filename)
+    subprocess.call(f"ffmpeg -i {data} -ss {start} -to {end} {filename}/%5d.png",shell=True,stdout=DEVNULL,stderr=DEVNULL)
+    subprocess.call(f"ffmpeg -i {data} -ss {start} -to {end} {filename}/audio.wav",shell=True,stdout=DEVNULL,stderr=DEVNULL)
+    return [filename]
 def get_landmark_bbox(data:np.ndarray,parameters:dict):
     max_h,max_w=data.shape[:-1]
     landmarks,bboxes=fa.get_landmarks(data,return_bboxes=True)
@@ -55,21 +61,19 @@ def get_landmark_bbox(data:np.ndarray,parameters:dict):
     return data,parameters
 def eye_dist(data,parameters):
     eye_dist=0
-    if parameters["valid"]:
-        landmark=parameters["landmark"]
-        left_eye=(landmark[39]+landmark[36])//2
-        right_eye=(landmark[45]+landmark[42])//2
-        x_dist,y_dist=right_eye-left_eye
-        eye_dist=(y_dist**2+x_dist**2)**0.5
+    landmark=parameters["landmark"]
+    left_eye=(landmark[39]+landmark[36])//2
+    right_eye=(landmark[45]+landmark[42])//2
+    x_dist,y_dist=right_eye-left_eye
+    eye_dist=(y_dist**2+x_dist**2)**0.5
     if eye_dist < 80:
         parameters["valid"]=False
         raise ValidException()
     return data,parameters
 def crop_data(data,parameters):
-    if parameters["valid"]:
-        bbox=parameters["bbox"].astype(np.int16)
-        data=data[bbox[1]:bbox[3],bbox[0]:bbox[2]]
-        return data,parameters
+    bbox=parameters["bbox"].astype(np.int16)
+    data=data[bbox[1]:bbox[3],bbox[0]:bbox[2]]
+    return data,parameters
 def get_angle(data,parameters):
     if parameters["valid"]:
         landmark=parameters["landmark"]
@@ -98,34 +102,48 @@ def rotate_image(data,parameters):
         M[1,2]+=(ty-rotate_center[1])
         data=cv2.warpAffine(data,M,(int(fw),int(fh)))
     return data,parameters
+
+def move_data(data,parameters):
+    shutil.move(data,parameters)
+
+
 if __name__=="__main__":
-    parameters={"valid":True,"is_down":True}
-    video_list=["test.mp4"]
-    for item in video_list:
-        landmark_list=[]
-        angle_list=[]
-        video=cv2.VideoCapture(item)
-        dirname=item.rsplit(".mp4")[0]
-        os.makedirs(dirname,exist_ok=True)
+    job_list=["test.mp4","https://www.youtube.com/watch?v=xWTiOqJqkk0"]
+    parameters_list=[{"valid":True,"is_download":False,"is_avspeech":False},
+        {"valid":True,"is_download":True,"is_avspeech":True}]
+    for  i,item in enumerate(job_list):
         try:
-            count=1
-            while video.isOpened():
-                ret,frame=video.read()
-                if not ret:
-                    break
-                print(count)
-                data,parameters=get_landmark_bbox(frame,parameters)
-                
-                data,parameters=eye_dist(data,parameters)
-                data,parameters=crop_data(data,parameters)
-                data,parameters=get_angle(data,parameters)
-                landmark_list.append(parameters["landmark"])
-                angle_list.append(parameters["angle"])
-                save_path=dirname+"/"+str(count).zfill(5)+".png"
-                cv2.imwrite(save_path,data)
-                count+=1
-        except:
-            print("error")
-        if parameters["valid"]:
-            np.savez(f"{item}.npy",landmark=landmark_list,angle=angle_list)
-        
+            if "https://" in item and parameters_list[i]["is_download"]:
+                raw_path=download(item,parameters_list[i])
+            else:
+                raw_path=item
+            if parameters_list[i]["is_avspeech"]:
+                video_dir_list=avspeech_preprocess(raw_path,parameters_list[i])
+            else:
+                video_dir_list=voxceleb2_preproces(raw_path,parameters_list[i])
+            for video_dir in video_dir_list:
+                landmark_list=[]
+                angle_list=[]
+                bbox_list=[]
+                print(video_dir)
+                for frame_path in os.listdir(video_dir):
+                    parameters={"valid":True}
+                    if frame_path.endswith(".wav"):
+                        continue
+                    frame=cv2.imread(os.path.join(video_dir,frame_path))
+                    data,parameters=get_landmark_bbox(frame,parameters)
+                    data,parameters=eye_dist(data,parameters)
+                    data,parameters=crop_data(data,parameters)
+                    data,parameters=get_angle(data,parameters)
+                    landmark_list.append(parameters["landmark"])
+                    bbox_list.append(parameters["bbox"])
+                    angle_list.append(parameters["angle"])
+                    save_path=os.path.join(video_dir,frame_path)
+                    # print('save_path',save_path)
+                    cv2.imwrite(save_path,data)
+                if parameters_list[i]["valid"]:
+                    np.savez(f"{video_dir}",landmark=landmark_list,bbox=bbox_list,angle=angle_list)
+                    move_data(video_dir,"currect/")
+        except Exception as e:
+            print(e)
+            # print("is not valid")
